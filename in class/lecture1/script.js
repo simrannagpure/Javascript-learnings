32646d9f25e51ac6cfa2828de27527c79185e97a
Simran <simrannagpure96@gmail.com>
2023-09-22 02:57:08 +0530

    initial commit

---
+++ in class/lecture1/script.js
@@ -0,0 +1,12 @@
+//History - 1990's - Netscape created Javascript to get information in 10days then Microsoft collaborated
+//Ecmascript - 
+//To run Javascript in Browser - V8 engine for chrome, spiderman for mozilla
+//servers are computer without monitor, RAM ROM harddisk, fan
+//Nodejs creation - by the idea of making v8 engine as environment to write javascript
+//js engine - parses line one by one = both interpreted and compiled language
+//JS is capable of call API from server and update data
+//Browsers are event base, and JS can capture that
+
+//HW - DOM , CSSOM , RenderTree
+
+console.log("hello world")
